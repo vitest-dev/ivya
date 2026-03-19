dbb2d7a29fc570d88fe41171171315a9e9b435fc
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-03-19 16:01:12 +0900

    chore: setup vite-plus (#12)

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite-plus'
+
+export default defineConfig({
+  pack: {
+    entry: ['src/index.ts', 'src/publicUtils.ts', 'src/aria/index.ts'],
+    outDir: 'dist',
+    format: ['esm'],
+    fixedExtension: false,
+    tsconfig: './tsconfig.json',
+    target: 'es2018',
+    minify: false,
+    clean: true,
+    dts: true,
+  },
+  fmt: {
+    semi: false,
+    trailingComma: 'es5',
+    singleQuote: true,
+    bracketSameLine: true,
+    tabWidth: 2,
+    printWidth: 85,
+    ignorePatterns: [],
+  },
+  staged: {
+    '*': 'vp check --no-lint --fix',
+  },
+})
