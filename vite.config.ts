0522f4d7991c9b588082b386365036d68f64a0f4
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-03-19 11:41:58 +0900

    chore: remove tsdown.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,27 @@
-import tsdownConfig from './tsdown.config.js';
-
-import { defineConfig } from 'vite-plus';
+import { defineConfig } from 'vite-plus'
 
 export default defineConfig({
   staged: {
-    "*": "vp check --fix"
+    '*': 'vp check --fix',
   },
-  pack: tsdownConfig,
-  lint: {"options":{"typeAware":true,"typeCheck":true}},
+  pack: {
+    entry: ['src/index.ts', 'src/publicUtils.ts', 'src/aria/index.ts'],
+    outDir: 'dist',
+    format: ['esm'],
+    fixedExtension: false,
+    tsconfig: './tsconfig.json',
+    target: 'es2018',
+    minify: false,
+    clean: true,
+    dts: true,
+  },
   fmt: {
-    "semi": false,
-    "trailingComma": "es5",
-    "singleQuote": true,
-    "bracketSameLine": true,
-    "tabWidth": 2,
-    "printWidth": 85,
-    "ignorePatterns": []
+    semi: false,
+    trailingComma: 'es5',
+    singleQuote: true,
+    bracketSameLine: true,
+    tabWidth: 2,
+    printWidth: 85,
+    ignorePatterns: [],
   },
-});
+})
